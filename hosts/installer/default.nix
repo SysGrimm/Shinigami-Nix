b123e07c4dd5ffa27352897a7d857f5fee4e6d00
SysGrimm <sysgrimm@users.noreply.github.com>
2025-09-06 22:45:15 -0500

    fix: clean installer config to avoid conflicts with base ISO

--- hosts/installer/default.nix
+++ hosts/installer/default.nix
@@ -3,100 +3,12 @@
 {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
-    ../../modules/desktop/hyprland.nix
   ];
 
-  # Allow unfree packages (needed for firmware and some packages)
-  nixpkgs.config.allowUnfree = true;
+  # Enable Hyprland
+  programs.hyprland.enable = true;
 
-  # Allow unfree packages (needed for some firmware and drivers)
-  nixpkgs.config.allowUnfree = true;
-
-  # Override minimal config for our desktop environment
-  services.xserver.enable = lib.mkForce false;  # We're using Wayland
-  
-  # Enable Wayland and Hyprland
-  programs.hyprland = {
-    enable = true;
-    package = pkgs.hyprland;
-  };
-
-  # XDG portal for Wayland
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  };
-
-  # Wayland environment variables
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
-    MOZ_ENABLE_WAYLAND = "1";
-  };
-
-  # Make this ISO hardware-agnostic
-  isoImage = {
-    isoName = "nixos-hyprland-installer.iso";
-    makeEfiBootable = true;
-    makeUsbBootable = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    # Core desktop tools
-    foot  # Terminal
-    rofi-wayland  # Application launcher
-    waybar  # Status bar
-    mako  # Notifications
-    grim  # Screenshots
-    slurp  # Screen selection
-    wl-clipboard  # Clipboard
-    
-    # Applications
-    firefox  # Use Firefox instead of Brave for better compatibility
-    vscode
-    discord
-    steam
-    prismlauncher
-    
-    # System tools
-    tailscale
-    networkmanagerapplet
-    pavucontrol
-    brightnessctl
-    
-    # File management
-    xfce.thunar
-    xfce.thunar-volman
-  ];
-
-  # Enable Tailscale service
-  services.tailscale.enable = true;
-
-  # Hardware support
-  hardware.enableRedistributableFirmware = true;
-  hardware.bluetooth.enable = true;
-  
-  # Graphics and gaming support
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  # Audio via PipeWire (conflicts resolved)
-  sound.enable = false;  # Disable ALSA
-  hardware.pulseaudio.enable = false;  # Disable PulseAudio
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  # Display manager for Hyprland
+  # Enable Wayland compositor essentials
   services.greetd = {
     enable = true;
     settings = {
@@ -107,20 +19,50 @@
     };
   };
 
-  # Network
-  networking.networkmanager.enable = true;
-
-  # Set a default user for live session
-  users.users.nixos = {
-    isNormalUser = true;
-    password = "nixos";
-    extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
+  # Audio support
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
   };
 
-  # Allow sudo without password for live user
-  security.sudo.wheelNeedsPassword = false;
+  # Enable Tailscale
+  services.tailscale.enable = true;
 
-  # Set a default locale and timezone
-  i18n.defaultLocale = "en_US.UTF-8";
-  time.timeZone = "UTC";
+  # Hardware support
+  hardware.bluetooth.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+
+  # Your requested applications
+  environment.systemPackages = with pkgs; [
+    # Desktop essentials
+    foot
+    waybar
+    rofi-wayland
+    mako
+    grim
+    slurp
+    wl-clipboard
+    
+    # Your applications
+    brave
+    vscode
+    discord
+    steam
+    prismlauncher
+    chiaki-ng
+    
+    # System tools
+    tailscale
+    git
+    curl
+    wget
+    neovim
+    networkmanagerapplet
+    pavucontrol
+    brightnessctl
+    xfce.thunar
+  ];
+
+  # Live session user (inherits from minimal CD)
+  users.users.nixos.extraGroups = [ "video" "audio" ];
 }
